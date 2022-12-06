3d3109274768517dc886b2ee955eb6ae20d626ee
aumcintyre <aumcintyre@gmail.com>
2022-12-06 08:21:55 -0600

    syling changes

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -3,8 +3,7 @@ import React from 'react';
 function Header({ currentPage, handlePageChange }) {
     return (
         <>
-            <header>
-                This is my header!
+            <header className='header-box'>
                 <ul id='nav' className='nav'>
                     <li className='active'><a href='#home' onClick={() => handlePageChange('Home')} className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Home</a></li>
                     <li className='active'><a href='#about' onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About Me</a></li>
